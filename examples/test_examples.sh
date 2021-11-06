deadf819df3486b17588886c6092fd50939255c3
invakid404 <invakid404@riseup.net>
2021-11-06 21:12:42 +0200

    chore(examples): add test cases and runner script

---
+++ examples/test_examples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for src_file in *.xml; do
+	src_filename=$(basename "${src_file}" .xml)
+	obj_file="${src_filename}.o"
+
+	tobsterlang ${src_file} -o ${obj_file} -O3 >/dev/null
+	clang-11 -o ${src_filename} ${obj_file} >/dev/null
+
+	in_file="${src_filename}.input"
+	out_file="${src_filename}.output"
+
+	diff "${out_file}" <(./"${src_filename}" < "${in_file}") || { echo "${src_filename} produced incorrect output"; exit 1; }
+done
